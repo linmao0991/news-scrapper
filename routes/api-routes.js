823b7dde9b448461b90a299cec292a5a4549d384
Mao <lin.maoxian0991@gmail.com>
2020-01-02 17:01:34 -0500

    finished scrap function for washington post

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -8,53 +8,81 @@ mongoose.connect("mongodb://localhost/newsarticlesdb", { useNewUrlParser: true }
 module.exports = function (app) {
     app.get("/scrapetheearticles", function (req, res){
         wapoScrape();
-        //reutersScrape();
+        reutersScrape();
     });
 }
 
 function wapoScrape(){
     axios.get("https://www.washingtonpost.com").then(function(response){
         var $ = cheerio.load(response.data);
-        var site = "https://www.washingtonpost.com"
+        var site = "Washington Post";
+        var url = "https://www.washingtonpost.com";
 
-        $("div.pb-feature").each(function(i, element){
-            
+        $("div[moat-id='homepage/story-ans']").each(function(i, element){
             var result = {}
-            if($(this).find("li.byline")){
-                var tempVar = $(this).find("li.byline").find("span").each(function(){
-                    tempVar =  tempVar + $(this).text();
-                })
-                console.log(tempVar);
+            result.site = site;
+            result.title = $(this).find(".headline").children("a").text().trim();
+            result.link = $(this).find(".headline").children("a").attr("href");
+            if(result.title.length = 0){
+                result.title = false;
+                result.link = false;
+            }
+            result.timestamp = $(this).find("li.timestamp").text();
+            result.snippet = $(this).find("div.blurb").text();
+            if(result.snippet.length === 0){
+                result.snippet = "No Blurb";
+            }
 
-                // result.site = site;
-                // result.title = $(this).find("h1.headline").text();
-                // result.link = $(this).find("a").attr("href");
-                // result.timestamp = $(this).find("li.timestamp").text();
-                // result.snippet = $(this).find("div.blurb").text();
-            
-                // db.articles.create(result).then(function(dbArticle){
-                //     console.log(dbArticle);
-                // }).catch(function(err){
-                //     console.log(err);
-                // });
-                console.log("True");
+            if(result.title && result.link){
+                db.articles.create(result).then(function(dbArticle){
+                    //console.log(dbArticle);
+                }).catch(function(err){
+                    console.log(err);
+                });
+                console.log(result);
             }else{
-                console.log("False");
+                console.log("No Article")
+                console.log("Skip insert into database");
             }
         });
+        console.log("Complete");
     });
 }
 
 function reutersScrape(){
     axios.get("https://www.reuters.com").then(function(response){
-        var site = "https://www.reuters.com"
+        var site = "Reuters"
+        var url = "https://www.reuters.com"
         var $ = cheerio.load(response.data);
 
+        //Top Story
+        $("section#topStory").each(function(i, element){
+            var result = {}
+            result.site = site;
+            result.title = $(this).find("h2.story-title").children("a").text().trim();
+            result.link = url+$(this).find("h2.story-title").children("a").attr("href");
+            result.timestamp = $(this).find("span.timestamp").text();
+            if(result.timestamp){
+                console.log("True")
+            }else{
+                console.log("No Time Stamp");
+                result.timestamp = "No Time Stamp";
+            }
+            result.snippet = $(this).find("p").text();
+        
+            db.articles.create(result).then(function(dbArticle){
+                console.log(dbArticle);
+            }).catch(function(err){
+                console.log(err);
+            });
+        });
+
+        //Top news
         $("article.story").each(function(i, element){
             var result = {}
             result.site = site;
             result.title = $(this).children("div.story-content").children("a").children("h3").text().trim();
-            result.link = site+$(this ).children("div.story-content").children("a").attr("href");
+            result.link = url+$(this ).children("div.story-content").children("a").attr("href");
             result.timestamp = $(this).find("span.timestamp").text();
             result.snippet = $(this).find("p").text();
         
@@ -64,5 +92,6 @@ function reutersScrape(){
                 console.log(err);
             });
         });
+        console.log("Complete");
     });
 }
