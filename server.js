5dbd8f876804fc07b637b3bb4208662250b48e9d
Mao <lin.maoxian0991@gmail.com>
2020-01-06 23:53:00 -0500

    Fixing issue with ports on heroku

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require("express");
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 var app = express();
 var mongoose = require("mongoose");
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/newsarticlesdb"
